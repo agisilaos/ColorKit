90fa0ff9e8cf9b6017e80a7ce7b971f2bf8bab20
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2025-03-12 10:57:18 +0100

    Update ColorKit.swift

--- Sources/ColorKit/ColorKit.swift
+++ Sources/ColorKit/ColorKit.swift
@@ -6,7 +6,7 @@ import SwiftUI
 /// ColorKit is a comprehensive color utility library for SwiftUI
 public enum ColorKit {
     /// The current version of ColorKit
-    public static let version = "1.0.0"
+    public static let version = "1.1.0"
     
     /// WCAG Compliance Checker
     public struct WCAG {
