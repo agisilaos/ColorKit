4b47d9e5c86a8b0d96989faa1c6089298cfd78dc
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2025-03-25 19:33:15 +0100

    refactor: remove duplicate HSL conversion code from WCAGCompliance
    - Remove duplicate fromHSL static function from WCAGCompliance.swift
    
    - Update WCAGColorSuggestions to use Color(hue:saturation:lightness:) initializer
    
    - Improve code organization by keeping HSL operations in Color+HSL.swift

--- Sources/ColorKit/WCAG/WCAGColorSuggestions.swift
+++ Sources/ColorKit/WCAG/WCAGColorSuggestions.swift
@@ -62,12 +62,11 @@ public struct WCAGColorSuggestions {
                     ? max(0.0, hsl.lightness - (Double(i) / Double(steps)) * hsl.lightness)
                     : min(1.0, hsl.lightness + (Double(i) / Double(steps)) * (1.0 - hsl.lightness))
 
-                if let adjustedColor = Color.fromHSL(hue: hsl.hue, saturation: hsl.saturation, lightness: CGFloat(adjustedLightness)) {
-                    let compliance = baseColor.wcagCompliance(with: adjustedColor)
-                    if compliance.passes.contains(targetLevel) {
-                        suggestions.append(adjustedColor)
-                        break
-                    }
+                let adjustedColor = Color(hue: hsl.hue, saturation: hsl.saturation, lightness: CGFloat(adjustedLightness))
+                let compliance = baseColor.wcagCompliance(with: adjustedColor)
+                if compliance.passes.contains(targetLevel) {
+                    suggestions.append(adjustedColor)
+                    break
                 }
             }
         }
@@ -77,15 +76,13 @@ public struct WCAGColorSuggestions {
         if suggestions.isEmpty {
             for saturationFactor in stride(from: 0.8, to: 0.0, by: -0.2) {
                 let adjustedSaturation = hsl.saturation * saturationFactor
-
                 let adjustedLightness = needsDarkening ? 0.0 : 1.0
 
-                if let adjustedColor = Color.fromHSL(hue: hsl.hue, saturation: adjustedSaturation, lightness: CGFloat(adjustedLightness)) {
-                    let compliance = baseColor.wcagCompliance(with: adjustedColor)
-                    if compliance.passes.contains(targetLevel) {
-                        suggestions.append(adjustedColor)
-                        break
-                    }
+                let adjustedColor = Color(hue: hsl.hue, saturation: adjustedSaturation, lightness: CGFloat(adjustedLightness))
+                let compliance = baseColor.wcagCompliance(with: adjustedColor)
+                if compliance.passes.contains(targetLevel) {
+                    suggestions.append(adjustedColor)
+                    break
                 }
             }
         }
